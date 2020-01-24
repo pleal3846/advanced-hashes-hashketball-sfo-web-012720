1f87868da3aba28bde9b32e24d63b250f842dd27
patrick leal <pjleal3846@icloud.com>
2020-01-24 22:07:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
     :away => { :team_name => "charlotte hornets",
     :colors => ["Turquoise", "Purple"],
     :players => [
-      {}]
+      {:player_name}]
     # Write your code here!
 
 
